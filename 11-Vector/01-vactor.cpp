84e42ca5959ea824c75b9fcdda267ba4b64dcd55
Ratnesh Kumar <rk02ratnesh@gmail.com>
2026-08-11 23:21:23 +0530

    basics of vector

---
+++ 11-Vector/01-vactor.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+    //syntax
+    vector<int> arr(5, -1); // 5 is size of vector and -1 is default value
+
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr.push_back(10); //adds an element at last 
+    arr.push_back(12);
+
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr.pop_back(); //delete an element at last 
+
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
