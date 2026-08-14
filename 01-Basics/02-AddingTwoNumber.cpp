fe43333cc5c2aa76180d61b9178165f590e16dbd
Ratnesh Kumar <rk02ratnesh@email.com>
2026-08-14 14:49:48 +0530

    basics added

---
+++ 01-Basics/02-AddingTwoNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, sum;
+
+    cout << "Enter two number:";
+    cin >> a >> b;
+
+    sum = a + b;
+    cout << "Addition is : " << sum << endl;
+
+    return 0;
+}
---
+++ 01-Basics/03-PrintWelcomeMsg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+
+    cout << "May I know your name";
+    getline(cin, name);
+
+    cout << "Welcome " << name << endl;
+
+    return 0;
+}
